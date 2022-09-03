3d8ec05565fadb7a79081a7013d72d498f7c93bb
gameplayer12122 <107828356+gameplayer12122@users.noreply.github.com>
2022-09-03 01:48:41 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ cd C:\hostedtoolcache\windows\Python
 del /Q 3.7.9
 del /Q 3.8.10
 del /Q 3.9.13
-cd C:\Users\%userprofile%\LizardScraper
+cd C:\Users\runneradmin\LizardScraper
 REM Creating a Newline variable (the two blank lines are required!)
 set NLM=^
 
@@ -14,11 +14,10 @@ pip install google
 pip install colorama
 pip install validators
 pause
-echo clear%NL%pause%NL%python LizardScraper.py >C:\Users\%userprofile%\LizardScraper\open.txt
+echo clear%NL%pause%NL%python LizardScraper.py>C:\Users\runneradmin\LizardScraper\open.txt
+pause
 ren open.txt open.bat
 clear
 start open.bat
-pause
 del /Q run.bat
-
-
+taskkill -F run.bat
